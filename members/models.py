0a1726f609d4ae4c67b2bf43e936625b2e66b93b
Khalil Hasan <kalilhasan970@gmail.com>
2023-05-10 17:14:08 +0100

    sdfa

--- members/models.py
+++ members/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Accounts(models.Model):
+
+    fname  = models.CharField(max_length=50)
+    lname =  models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zip = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.fname
+
+    class Meta :
+        verbose_name = "Accounts"
+        ordering =["fname"]
--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('members/', include('members.urls')),
-    path('', include('login.urls')),
+    path('', include('members.urls')),
+    path('login/', include('login.urls')),
 ]
